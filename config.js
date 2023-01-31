b169c6db793804cc118b93204702b1701fb8809e
Nailington <111466491+Nailington@users.noreply.github.com>
2023-01-31 11:55:43 -0500

    You may still use the green theme!!!

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 let theme = "green";
 let useCustomTheme = true; // Must Set to true if using a custom theme
-let customHost = "https://math-study.github.io/gba-host/";
-let useCustomHost = true; // Must Set to true if using a custom host
+let customHost = "";
+let useCustomHost = false; // Must Set to true if using a custom host
 let useAltHost = false; 
 let useNetPlay = false;
 let useAltNdsHost = false;
